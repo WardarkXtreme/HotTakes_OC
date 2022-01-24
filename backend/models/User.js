3e171706de4254b229831e22a013d56e86c4c496
WardarkXtreme <biard.f.pro@gmail.com>
2022-01-24 14:18:13 +0100

    mise en place du models mongo pour les comptes Utilisateurs

---
+++ backend/models/User.js
@@ -0,0 +1,12 @@
+//***---Déclaration des variable contenant les packages requis---***/
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+//***---Initialisation de la constante user schema qui contient le type de format attendu pour le User---***/
+const userSchema = mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+//***---la constante userSchema va être controller par le plugin unique validator---***//
+userSchema.plugin(uniqueValidator);
+//maintenant nous exportons ce models pour qu'il puisse servir lors de l'appel---***//
+module.exports = mongoose.model('User', userSchema);
