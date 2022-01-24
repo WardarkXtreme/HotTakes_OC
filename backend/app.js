c108d8ad0f366dedb41d65e0cfa92e062fb9de2a
WardarkXtreme <biard.f.pro@gmail.com>
2022-01-24 13:55:03 +0100

    mise en place de la connexion à la base de donnée

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,18 @@
-//initialisation des ressources nécessaire au bon fonctionnement de l'API//
+//***---initialisation des ressources nécessaire au bon fonctionnement de l'API---***//
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-//initialisation de la variable app qui contient la fonction express
+//***---initialisation de la variable app qui contient la fonction express---***/
 const app = express();
+//***---initialisation du chemin d'accées de mongo, et des paramêtre de conection---***/
+const srcDb = 'mongodb+srv://flodev:flo001@cluster0.l4gnm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+//***---Connexion a la base de donnée mongoDb---***/
+mongoose.connect(srcDb, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("la connexion est étabie"))
+    .catch(() => console.log("Une erreur est survenue lors de votre connexion à mongoDb"))
+;
 
-
+//***---Package installée => express => mongoose => bodyParser => path---***/
 module.exports = app;
