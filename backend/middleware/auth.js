bce19690a02d8fa8d962713c102d316f71bf913a
WardarkXtreme <biard.f.pro@gmail.com>
2022-01-24 15:41:37 +0100

    mise en place de la logique token (middleware)

---
+++ backend/middleware/auth.js
@@ -0,0 +1,19 @@
+//***---Déclaration des variable contenant les packages requis---***/
+const jwt = require('jsonwwebtoken');
+//***---Exportation de la fonction de création de token pour l'utilisateur---***/
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, `RDM_TOKEN_SECRET`);
+        const userId = decodedToken.userId;
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'Cet utilisateur est inconnu.';
+        } else {
+            next();
+        }
+    }
+    catch (error) {
+        res.status(401).json({ message: 'La requête ne peut aboutir, vérifiez vos informations de connexion.' }),
+        error => console.log(error)
+    }
+}
