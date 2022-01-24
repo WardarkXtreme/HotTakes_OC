867c7bead74ada2ecf917b76c5fbb82e7f01b8cd
WardarkXtreme <biard.f.pro@gmail.com>
2022-01-24 14:47:12 +0100

    mise en place du Models sauce

--- backend/app.js
+++ backend/app.js
@@ -20,5 +20,5 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-//***---Package installée => express => mongoose => bodyParser => path---***/
+//***---Package installée => express => mongoose => bcrypt => multer => bodyParser => mongoose unique validator => path---***/
 module.exports = app;
---
+++ backend/models/Sauce.js
@@ -0,0 +1,18 @@
+//***---Déclaration des variable contenant les packages requis---***/
+const mongoose = require('mongoose');
+//***---Initialisation de la constante SauceSchema qui contient le type de format attendu pour la Sauce---***/
+const sauceSchema = mongoose.Schema({
+    userId: {type: String, required: true},
+    name: {type: String, required: true},
+    manufacturer: {type: String, required: true},
+    description: {type: String, required: true},
+    mainPepper: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    heat: {type: Number, required: true},
+    likes: {type: Number, required: false, default:0},
+    dislikes: {type: Number, required: false, default:0},
+    usersLiked: {type: String, required: false, default:[]},
+    usersDisliked: {type: String, required: false, default:[]},
+});
+//***---maintenant nous exportons ce models pour qu'il puisse servir lors de l'appel---***/
+module.exports = mongoose.model('Sauce', sauceSchema);
