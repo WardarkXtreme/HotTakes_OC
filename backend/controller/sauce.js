c89ad07e087f293d4f8a102b4a7b0a103716fced
WardarkXtreme <biard.f.pro@gmail.com>
2022-01-26 21:48:36 +0100

    revue de la modification de la sauce

--- backend/controller/sauce.js
+++ backend/controller/sauce.js
@@ -35,16 +35,17 @@ exports.createSauce = (req, res, next) => {
 
 //***---Controlleur de modification de sauce---***/
 exports.modifySauce = (req, res, next) => {
+    
+    if(req.file) {
+        Sauce.findOne({ _id : req.params.id})
+        .then(sauce => {
+            const verify = JSON.parse(req.body.sauce);
+            if(sauce.userId != verify.userId) {
+                return res.status(401).json({ message: 'il semblerait que cette sauce ne soit pas à vous.'})
+            }else {
 
-    const verify = JSON.parse(req.body.sauce);
-    Sauce.findOne({ _id : req.params.id})
-    .then(sauce => {
-        if(sauce.userId != verify.userId) {
-            res.status(401).json({ message: 'il semblerait que cette sauce ne soit pas à vous.'})
-        }else {
-            if(req.file) {
                 const filename = sauce.imageUrl.split('/images/')[1];
-                const getModifySauce = JSON.parse(req.body.sauce);
+                let getModifySauce = JSON.parse(req.body.sauce);
                 fs.unlink(`images/${filename}`, () => {
                     let newModification = {
                         ...getModifySauce,
@@ -54,15 +55,16 @@ exports.modifySauce = (req, res, next) => {
                     .then(() => res.status(200).json({ message: 'Modification effectuée avec succés.' }))
                     .catch(err => res.status(400).json({ err }));
                 })
-            }else {
-                let newModification = { ...req.body};
-                Sauce.updateOne({ _id: req.params.id }, { ...newModification, _id: req.params.id })
-                .then(() => res.status(200).json({ message: 'Modification effectuée avec succés.' }))
-                .catch(err => res.status(400).json({ err }));
             }
-        }
-    })
-    .catch(err => res.status(500).json({ err }));
+        })
+        .catch(err => res.status(500).json({ err }));
+    }else {
+        const newModification = {...req.body};
+        Sauce.updateOne({ _id: req.params.id }, { ...newModification, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Modification effectuée avec succés.' }))
+        .catch(err => console.log(err));
+    };
+
 };
 
 //***---Controlleur de suppression de sauce---***/
